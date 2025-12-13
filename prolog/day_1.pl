46d502c14c86c1d465e89edba5d03ff2960ed89b
Edgar Nova <ragnarok540@gmail.com>
2025-12-13 10:47:10 -0500

    prolog day 1

---
+++ prolog/day_1.pl
@@ -0,0 +1,25 @@
+author(tolkien, lotr).
+author(martin, got).
+author(tolkien, hobbit).
+author(herbert, dune).
+
+% author(tolkien, X).
+
+plays(mary, guitar).
+plays(andrew, keyboard).
+plays(lex, guitar).
+plays(mary, drums).
+plays(paul, bass).
+plays(andrew, guitar).
+
+genre(mary, rock).
+genre(andrew, pop).
+genre(lex, rock).
+genre(paul, punk).
+genre(paul, ska).
+
+genre_plays(X, Y, Z) :-
+    genre(Z, X),
+    plays(Z, Y).
+
+% genre_plays(X, guitar, Z).
