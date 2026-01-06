a8283a9333ec2b981e8710b4551e214f42e57a06
Edgar Nova <ragnarok540@gmail.com>
2026-01-06 18:55:15 -0500

    clojure day 3 start

---
+++ clojure/day_3.clj
@@ -0,0 +1,29 @@
+;; • Use refs to create a vector of accounts in memory. Create debit
+;; and credit functions to change the balance of an account.
+
+(def account1 (ref 50))
+(def account2 (ref 100))
+
+(defn credit [account value]
+    (dosync
+        (alter account + value)
+        value))
+
+(defn debit [account value]
+    (dosync
+        (alter account - value)
+        value))
+
+(defn transfer-money [a1 a2 value]
+    (dosync
+        (alter a1 - value)
+        (alter a2 + value)
+        value))
+
+(println @account1 @account2)
+(credit account1 100)
+(println @account1 @account2)
+(debit account2 50)
+(println @account1 @account2)
+(transfer-money account1 account2 50)
+(println @account1 @account2)
